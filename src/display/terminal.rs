0d8b5c6a52e0f7c54c22f8f22d8c5474b15b61e5
Alexandre Otávio Guedes Drummond <aod@ngrt.org>
2026-07-05 14:40:29 -0300

    ...

--- src/display/terminal.rs
+++ src/display/terminal.rs
@@ -23,39 +23,43 @@ impl DisplaySource for TerminalDisplay {
         let mut band_acc = vec![0.0f32; 8];
         let mut count = 0;
 
-        while let Ok(bands) = rx_bands.recv() {
-
-            for i in 0..bands.samples.len() {
-                band_acc[i] += bands.samples[i];
-            }
-
-            count += 1;
-            if last_update.elapsed() >= Duration::from_secs(1)
-                && count > 0
-            {
-                last_update = Instant::now();
-
-                let mut avg = vec![0.0; 8];
-
-                for i in 0..8 {
-                    avg[i] = to_db_display(
-                            band_acc[i] / count as f32
-                        );
+    while !shutdown.load(Ordering::SeqCst) {
+        match rx_bands.recv_timeout(Duration::from_millis(100)) {
+            Ok(frame) => {
+                for i in 0..bands.samples.len() {
+                    band_acc[i] += bands.samples[i];
                 }
 
-                print!("Bands: ");
+                count += 1;
+                if last_update.elapsed() >= Duration::from_secs(1)
+                    && count > 0
+                {
+                    last_update = Instant::now();
 
-                for (i, v) in avg.iter().enumerate() {
-                    print!("B{}: {:.2}dB ; ", i + 1, v);
+                    let mut avg = vec![0.0; 8];
+
+                    for i in 0..8 {
+                        avg[i] = to_db_display(
+                                band_acc[i] / count as f32
+                            );
+                    }
+
+                    print!("Bands: ");
+
+                    for (i, v) in avg.iter().enumerate() {
+                        print!("B{}: {:.2}dB ; ", i + 1, v);
+                    }
+
+                    println!();
+                    band_acc.fill(0.0);
+                    count = 0;
                 }
+            
 
-                println!();
-                band_acc.fill(0.0);
-                count = 0;
+                println!("Display finished");
+                }
+            Err(mpsc::RecvTimeoutError::Timeout) => continue,
+            Err(mpsc::RecvTimeoutError::Disconnected) => break,
             }
-        }
-
-        println!("Display finished");
     }
-
 }
