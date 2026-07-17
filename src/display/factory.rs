c6c222d6ac54d2b4e3c0877de7cea1cda9384243
Alexandre Otávio Guedes Drummond <aod@ngrt.org>
2026-07-16 23:09:24 -0300

    fixing compilation error

--- src/display/factory.rs
+++ src/display/factory.rs
@@ -1,5 +1,10 @@
 use std::error::Error;
 use log::{info,trace,error,warn};
+use std::sync::{
+    Arc,
+    atomic::{AtomicBool},
+    mpsc
+};
 
 use super::{
     source::DisplaySource,
@@ -80,4 +85,14 @@ where
             initialize_display_source(backup()?)
         }
     }
+}
+
+fn display_results(
+    mut source: Box<dyn display::source::DisplaySource>,
+    rx_bands: mpsc::Receiver<AudioFrame>,
+    shutdown: Arc<AtomicBool>,
+)
+{
+    info!("Initiating display thread.");
+    source.display_results(rx_bands, shutdown);
 }
--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use audio_processing::process_audio;
 use audio::source::AudioFrame;
 use utils::init_args::parse_args;
 use audio::source::AudioSource;
-use display::source::DisplaySource;
+use display::factory::display_results;
 
 fn produce_audio(
     mut source: Box<dyn audio::source::AudioSource>,
@@ -49,15 +49,15 @@ fn produce_audio(
     }
 }
 
-fn display_results(
-    mut source: Box<dyn display::source::DisplaySource>,
-    rx_bands: mpsc::Receiver<AudioFrame>,
-    shutdown: Arc<AtomicBool>,
-)
-{
-    info!("Initiating display thread.");
-    source.display_results(rx_bands, shutdown);
-}
+// fn display_results(
+//     mut source: Box<dyn display::source::DisplaySource>,
+//     rx_bands: mpsc::Receiver<AudioFrame>,
+//     shutdown: Arc<AtomicBool>,
+// )
+// {
+//     info!("Initiating display thread.");
+//     source.display_results(rx_bands, shutdown);
+// }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     
